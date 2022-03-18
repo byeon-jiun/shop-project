0903dd75d313f4292c77a9804989abb293d885eb
byeonjiun <askywsa@smartscore.kr>
2022-03-18 17:55:24 +0900

    연관관계 매핑 및 영속성 전이 및 지연로딩 테스트 코드 작성 auditing 이용 엔티티 공통속성 공통화

---
+++ shop/src/test/java/com/shop/repository/CartRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.shop.repository;
+
+import com.shop.dto.MemberFromDto;
+import com.shop.entity.Cart;
+import com.shop.entity.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+class CartRepositoryTest {
+
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @PersistenceContext
+    EntityManager em;
+
+    public Member createMember(){
+        MemberFromDto memberFromDto = new MemberFromDto();
+        memberFromDto.setEmail("test@email.com");
+        memberFromDto.setName("변지운");
+        memberFromDto.setAddress("서울시 삼성동");
+        memberFromDto.setPassword("1234");
+        return Member.createMember(memberFromDto, passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("장바구니 회원 엔티티 매핑 조회 테스트")
+    public void findCartAndMemberTest(){
+        Member member = createMember();
+        memberRepository.save(member);
+
+        Cart cart = new Cart();
+        cart.setMember(member);
+        cartRepository.save(cart);
+
+        em.flush();
+        em.clear();
+
+        Cart savedCart = cartRepository.findById(cart.getId()).orElseThrow(EntityNotFoundException::new);
+        assertEquals(savedCart.getMember().getId(), member.getId());
+    }
+}
